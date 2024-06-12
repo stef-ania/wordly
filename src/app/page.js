7f91a8eb8e9cb4d8213bbfa62c48c43d8f955022
stef-ania <stebofof@gmail.com>
2024-06-12 10:02:47 +0200

    Changed html tag

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Wordly</p>
+        <h1>Wordly</h1>
       </div>
     </main>
   );
