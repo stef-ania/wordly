f974dfa470817b3e77839868b150e398e78ab124
stef-ania <stebofof@gmail.com>
2024-06-12 15:43:09 +0200

    Add API connection with Axios

--- src/app/Components/Dictionary.jsx
+++ src/app/Components/Dictionary.jsx
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary() {
@@ -9,10 +10,26 @@ export default function Dictionary() {
     alert(`searching for ${word}`);
   }
 
+  let API_KEY = `${process.env.NEXT_PUBLIC_API_KEY}`;
+
+  let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${API_KEY}`;
+
+  axios
+    .get(apiURL)
+    .then(handleResponse)
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+
+  function handleResponse(res) {
+    console.log(res.data);
+  }
+
   function handleWordChange(e) {
     console.log(e.target.value);
     setWord(e.target.value);
   }
+
   return (
     <>
       <form onSubmit={search}>
