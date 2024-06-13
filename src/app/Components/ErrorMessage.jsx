eb4a2d052bce24d099d9e5f1320a417b8b83989b
stef-ania <stebofof@gmail.com>
2024-06-13 15:51:30 +0200

    Install styled-components and style error message

--- src/app/Components/ErrorMessage.jsx
+++ src/app/Components/ErrorMessage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledErrorMessage = styled.p`
+  background-color: #f0d3cf;
+  border-radius: 12px;
+  color: #b02711;
+  font-weight: bold;
+  margin-top: 2rem;
+  padding: 1.5rem;
+`;
 
 export default function ErrorMessage({ message }) {
-  return <p>{message}</p>;
+  return <StyledErrorMessage>{message}</StyledErrorMessage>;
 }
