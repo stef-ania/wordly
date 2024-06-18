1c18eccdbfa51bbbb91217bd38372e944b43827b
stef-ania <stebofof@gmail.com>
2024-06-18 11:02:23 +0200

    Add text-to-speech in phonetic component

--- src/app/Components/DefinitionData.jsx
+++ src/app/Components/DefinitionData.jsx
@@ -4,6 +4,7 @@ import ErrorMessage from "./ErrorMessage.jsx";
 import styled from "styled-components";
 import { pt_serif } from "../utils/fonts.js";
 import { capitalizeFirstLetter } from "../utils/capitalizeFirstLetter";
+import Phonetic from "./Phonetic.jsx";
 
 const H3 = styled.h3`
   margin: 0rem 0 1rem;
@@ -13,10 +14,6 @@ const H3 = styled.h3`
   &:first-letter {
     text-transform: uppercase;
   }
-`;
-
-const Phonetic = styled.div`
-  margin: 2rem 0;
 `;
 
 const Separator = styled.hr`
@@ -33,10 +30,8 @@ export default function DefinitionData(props) {
       <h5>Searched word:</h5>
       <H3 className={pt_serif.className}>{capitalizeFirstLetter(props.definitionData.word)}</H3>
 
-      <Phonetic>
-        <span> Listen </span>
-        <em>/{props.definitionData.phonetic}/</em>
-      </Phonetic>
+      <Phonetic phonetic={props.definitionData.word} voiceName="Google UK English Female" rate={1} pitch={1.2} />
+      <em>/{props.definitionData.phonetic}/</em>
 
       {props.definitionData.meanings.map((meaning, index) => (
         <div key={index}>
--- src/app/Components/Phonetic.jsx
+++ src/app/Components/Phonetic.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
 
-// still WIP
+export default function Phonetic({ phonetic, voiceName = "Google US English", rate = 1, pitch = 1 }) {
+  const [utterance, setUtterance] = useState(null);
+  const [voices, setVoices] = useState([]);
 
-export default function Phonetic(props) {
+  useEffect(() => {
+    const synth = window.speechSynthesis;
+
+    const populateVoices = () => {
+      const availableVoices = synth.getVoices();
+      setVoices(availableVoices);
+    };
+
+    if (synth.onvoiceschanged !== undefined) {
+      synth.onvoiceschanged = populateVoices;
+    }
+
+    populateVoices();
+  }, []);
+
+  useEffect(() => {
+    if (phonetic && voices.length > 0) {
+      const synth = window.speechSynthesis;
+      const u = new SpeechSynthesisUtterance(phonetic);
+
+      const selectedVoice = voices.find((voice) => voice.name === voiceName);
+      if (selectedVoice) {
+        u.voice = selectedVoice;
+      }
+
+      u.rate = rate;
+      u.pitch = pitch;
+
+      setUtterance(u);
+
+      return () => {
+        synth.cancel();
+      };
+    }
+  }, [phonetic, voices, voiceName, rate, pitch]);
+
+  const handlePlay = () => {
+    if (utterance) {
+      const synth = window.speechSynthesis;
+      synth.speak(utterance);
+    }
+  };
+
   return (
     <div>
-      <span>
-        <a href={props.phonetic.audio}> Listen </a>
-      </span>
-      <em>/{props.definitionData.phonetic}/</em>
+      <button onClick={handlePlay}>{"Listen"}</button>
     </div>
   );
 }
